f77383bd0c199a2193dce03693a2589ae92c90f3
gustavo.pardini <ggpardini@gmail.com>
2024-11-12 10:24:20 -0300

    Iniciando projeto

---
+++ app/src/main/java/com/gustavo/appviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,17 @@
+package com.gustavo.appviagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gustavo.appviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+
+    }
+}
