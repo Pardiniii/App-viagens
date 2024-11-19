c1346b0b3d6cb2444aadf6b8e61abaf6a37273df
gustavo.pardini <ggpardini@gmail.com>
2024-11-19 16:05:55 -0300

    Criando tela de resumo de compra

---
+++ app/src/main/java/com/gustavo/appviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,22 @@
+package com.gustavo.appviagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.gustavo.appviagens.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resumo_pacote);
+
+    }
+}
